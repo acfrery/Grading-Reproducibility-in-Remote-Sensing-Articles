cd9246e461cfc6ede85e32e7a3510af1ca205ced
acfrery <acfrery@gmail.com>
2020-08-11 04:00:02 -0300

    R1 files

--- Figures/FreeMind/Badging RSRR.mm
+++ Figures/FreeMind/Badging RSRR.mm
@@ -1,163 +1,224 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1588014126305" ID="ID_1345263092" MODIFIED="1588103986380" STYLE="fork" TEXT="Badging RSRR">
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Badging RSRR" FOLDED="false" ID="ID_1345263092" CREATED="1588014126305" MODIFIED="1588103986380" STYLE="fork">
 <font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588014136447" ID="ID_1935949149" MODIFIED="1588103986380" POSITION="right" STYLE="fork" TEXT="Rationale">
-<edge STYLE="linear"/>
-<cloud/>
-<font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="full-1"/>
-</node>
-<node CREATED="1588014142060" ID="ID_1274549225" MODIFIED="1588103986380" POSITION="right" STYLE="fork" TEXT="Badging systems">
-<cloud/>
-<font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="full-2"/>
-<node CREATED="1588103792174" ID="ID_1949031882" MODIFIED="1588103986380" TEXT="IPOL">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-<node CREATED="1588015114104" ID="ID_192792286" MODIFIED="1588103986380" STYLE="fork" TEXT="Open Science">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-<node CREATED="1588015117335" ID="ID_553952589" MODIFIED="1588103986380" TEXT="ACM">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-</node>
-<node CREATED="1588014965504" ID="ID_838524198" MODIFIED="1588103986380" POSITION="right" STYLE="fork" TEXT="IEEE Platforms">
-<cloud/>
-<font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="full-3"/>
-<node CREATED="1588014995059" ID="ID_621269689" MODIFIED="1588103986379" TEXT="Code Ocean">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-<node CREATED="1588243376173" ID="ID_1160331423" MODIFIED="1588243383568" TEXT="Data Port"/>
-<node CREATED="1588015002264" ID="ID_920355450" MODIFIED="1588104045624" TEXT="Remote Sensing Code Library">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-</node>
-<node CREATED="1588014153977" ID="ID_1477740424" MODIFIED="1588103986379" POSITION="right" STYLE="fork" TEXT="Proposal">
-<font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="full-4"/>
-<node CREATED="1588014401589" ID="ID_1475201966" MODIFIED="1588103986379" TEXT="Requirements">
-<cloud/>
-<font NAME="Times New Roman" SIZE="14"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
 <icon BUILTIN="yes"/>
-<node CREATED="1588014406269" ID="ID_452144429" MODIFIED="1588103986379" TEXT="Web site">
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Rationale" POSITION="right" ID="ID_1935949149" CREATED="1588014136447" MODIFIED="1588103986380" STYLE="fork">
+<icon BUILTIN="full-1"/>
 <font NAME="Times New Roman" SIZE="14"/>
+<edge STYLE="linear"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+</node>
+<node TEXT="Badging systems" POSITION="right" ID="ID_1274549225" CREATED="1588014142060" MODIFIED="1588103986380" STYLE="fork">
+<icon BUILTIN="full-2"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="IPOL" ID="ID_1949031882" CREATED="1588103792174" MODIFIED="1588103986380">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Open Science" ID="ID_192792286" CREATED="1588015114104" MODIFIED="1588103986380" STYLE="fork">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="ACM" ID="ID_553952589" CREATED="1588015117335" MODIFIED="1588103986380">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+</node>
+<node TEXT="IEEE Platforms" POSITION="right" ID="ID_838524198" CREATED="1588014965504" MODIFIED="1588103986380" STYLE="fork">
+<icon BUILTIN="full-3"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Code Ocean" ID="ID_621269689" CREATED="1588014995059" MODIFIED="1588103986379">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Data Port" ID="ID_1160331423" CREATED="1588243376173" MODIFIED="1588243383568"/>
+<node TEXT="Remote Sensing Code Library" ID="ID_920355450" CREATED="1588015002264" MODIFIED="1588104045624">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+</node>
+<node TEXT="Proposal" POSITION="right" ID="ID_1477740424" CREATED="1588014153977" MODIFIED="1588103986379" STYLE="fork">
+<icon BUILTIN="full-4"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Requirements" ID="ID_1475201966" CREATED="1588014401589" MODIFIED="1588103986379">
+<icon BUILTIN="yes"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Web site" ID="ID_452144429" CREATED="1588014406269" MODIFIED="1588103986379">
 <icon BUILTIN="flag-blue"/>
-</node>
-<node CREATED="1588015069207" ID="ID_459158239" MODIFIED="1588103986379" TEXT="Experimental design">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588014411443" ID="ID_1345951920" MODIFIED="1588103986379" TEXT="Open data">
+<node TEXT="Experimental design" ID="ID_459158239" CREATED="1588015069207" MODIFIED="1588103986379">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588015035578" ID="ID_354462007" MODIFIED="1588103986379" TEXT="FLOSS">
-<font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588015046389" ID="ID_1351208651" MODIFIED="1588103986379" TEXT="Partial">
-<cloud/>
-<arrowlink COLOR="#3333ff" DESTINATION="ID_1294571765" ENDARROW="Default" ENDINCLINATION="300;0;" ID="Arrow_ID_1212358563" STARTARROW="Default" STARTINCLINATION="300;0;"/>
+<node TEXT="Open data" ID="ID_1345951920" CREATED="1588014411443" MODIFIED="1588103986379">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588015051093" ID="ID_868253952" MODIFIED="1588103986379" TEXT="Full">
-<cloud/>
-<arrowlink COLOR="#660033" DESTINATION="ID_574495131" ENDARROW="Default" ENDINCLINATION="271;13;" ID="Arrow_ID_1516132616" STARTARROW="Default" STARTINCLINATION="256;13;"/>
+<node TEXT="FLOSS" ID="ID_354462007" CREATED="1588015035578" MODIFIED="1588103986379">
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Partial" ID="ID_1351208651" CREATED="1588015046389" MODIFIED="1588103986379">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#3333ff" WIDTH="2" TRANSPARENCY="255" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1294571765" STARTINCLINATION="300;0;" ENDINCLINATION="300;0;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Full" ID="ID_868253952" CREATED="1588015051093" MODIFIED="1588103986379">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#660033" WIDTH="2" TRANSPARENCY="255" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_574495131" STARTINCLINATION="256;13;" ENDINCLINATION="271;13;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1588014416180" ID="ID_1087768159" MODIFIED="1588103986378" TEXT="Code">
+<node TEXT="Code" ID="ID_1087768159" CREATED="1588014416180" MODIFIED="1588103986378">
 <font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588014425469" ID="ID_1663098749" MODIFIED="1588103986378" TEXT="Analysis">
-<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Analysis" ID="ID_1663098749" CREATED="1588014425469" MODIFIED="1588103986378">
 <icon BUILTIN="xmag"/>
-</node>
-<node CREATED="1588014428858" ID="ID_182868439" MODIFIED="1588103986378" TEXT="Artifacts">
-<font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588014461348" ID="ID_1617100155" MODIFIED="1588103986378" TEXT="Plots">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588014463757" ID="ID_1421621638" MODIFIED="1588103986378" TEXT="Tables">
+<node TEXT="Artifacts" ID="ID_182868439" CREATED="1588014428858" MODIFIED="1588103986378">
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Plots" ID="ID_1617100155" CREATED="1588014461348" MODIFIED="1588103986378">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588014470788" ID="ID_844594145" MODIFIED="1588103986378" TEXT="Images">
+<node TEXT="Tables" ID="ID_1421621638" CREATED="1588014463757" MODIFIED="1588103986378">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Images" ID="ID_844594145" CREATED="1588014470788" MODIFIED="1588103986378">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
 </node>
 </node>
-<node CREATED="1588014420287" ID="ID_815884018" MODIFIED="1588103986378" TEXT="Text">
-<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Text" ID="ID_815884018" CREATED="1588014420287" MODIFIED="1588103986378">
 <icon BUILTIN="edit"/>
-</node>
-</node>
-<node CREATED="1588014157371" ID="ID_435999205" MODIFIED="1588103986378" TEXT="Implementation">
-<cloud/>
 <font NAME="Times New Roman" SIZE="14"/>
+</node>
+</node>
+<node TEXT="Implementation" ID="ID_435999205" CREATED="1588014157371" MODIFIED="1588103986378">
 <icon BUILTIN="go"/>
-<node CREATED="1588015260685" ID="ID_238638962" MODIFIED="1588103986378" TEXT="RSRR Committee">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
 <font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588015285444" ID="ID_303276720" MODIFIED="1588103986378" TEXT="Accepted papers">
+<node TEXT="RSRR Committee" ID="ID_238638962" CREATED="1588015260685" MODIFIED="1588103986378">
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Accepted papers" ID="ID_303276720" CREATED="1588015285444" MODIFIED="1588103986378">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588015291409" ID="ID_13036961" MODIFIED="1588103986378" TEXT="Upon authors&apos; request">
+<node TEXT="Upon authors&apos; request" ID="ID_13036961" CREATED="1588015291409" MODIFIED="1588103986378">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1588015196705" ID="ID_1336772483" MODIFIED="1588104126882" TEXT="Badges">
-<cloud/>
+<node TEXT="Badges" ID="ID_1336772483" CREATED="1588015196705" MODIFIED="1588104126882">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
 <font NAME="Times New Roman" SIZE="14"/>
-<node CREATED="1588015172839" ID="ID_574495131" MODIFIED="1588103986378" TEXT="Fully RSRR">
-<cloud/>
+<node TEXT="Fully RSRR" ID="ID_574495131" CREATED="1588015172839" MODIFIED="1588103986378">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<font NAME="Times New Roman" SIZE="14"/>
 <linktarget COLOR="#660033" DESTINATION="ID_574495131" ENDARROW="Default" ENDINCLINATION="271;13;" ID="Arrow_ID_1516132616" SOURCE="ID_868253952" STARTARROW="Default" STARTINCLINATION="256;13;"/>
-<font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588015184185" HGAP="21" ID="ID_1294571765" MODIFIED="1588103986377" TEXT="Partially RSRR" VSHIFT="7">
-<cloud/>
+<node TEXT="Partially RSRR" ID="ID_1294571765" CREATED="1588015184185" MODIFIED="1588103986377" HGAP_QUANTITY="21.0 px" VSHIFT_QUANTITY="7.0 px">
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<font NAME="Times New Roman" SIZE="14"/>
 <linktarget COLOR="#3333ff" DESTINATION="ID_1294571765" ENDARROW="Default" ENDINCLINATION="300;0;" ID="Arrow_ID_1212358563" SOURCE="ID_1351208651" STARTARROW="Default" STARTINCLINATION="300;0;"/>
+</node>
+<node TEXT="None" ID="ID_79697300" CREATED="1588015308587" MODIFIED="1588103986377">
 <font NAME="Times New Roman" SIZE="14"/>
 </node>
-<node CREATED="1588015308587" ID="ID_79697300" MODIFIED="1588103986377" TEXT="None">
-<font NAME="Times New Roman" SIZE="14"/>
 </node>
 </node>
 </node>
-</node>
-<node CREATED="1588018036908" ID="ID_1128781151" MODIFIED="1588103986377" POSITION="right" STYLE="fork" TEXT="How to start">
-<edge STYLE="bezier"/>
-<cloud/>
-<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="How to start" POSITION="right" ID="ID_1128781151" CREATED="1588018036908" MODIFIED="1588103986377" STYLE="fork">
 <icon BUILTIN="full-5"/>
-<node CREATED="1588018102754" ID="ID_1013290687" MODIFIED="1588103986377" TEXT="Git">
 <font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="gohome"/>
-</node>
-<node CREATED="1588018105879" ID="ID_1500768527" MODIFIED="1588103986377" TEXT="FreeMind">
-<font NAME="Times New Roman" SIZE="14"/>
-<icon BUILTIN="freemind_butterfly"/>
-</node>
-<node CREATED="1588103768886" ID="ID_1345977334" MODIFIED="1588103986377" TEXT="Slack">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-<node CREATED="1588103748814" ID="ID_445917738" MODIFIED="1588103986377" TEXT="Trello">
-<font NAME="Times New Roman" SIZE="14"/>
-</node>
-</node>
-<node CREATED="1588015786100" ID="ID_1597896197" MODIFIED="1588103986377" POSITION="right" STYLE="fork" TEXT="Challenges">
 <edge STYLE="bezier"/>
-<cloud/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Git" ID="ID_1013290687" CREATED="1588018102754" MODIFIED="1588103986377">
+<icon BUILTIN="gohome"/>
 <font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="FreeMind" ID="ID_1500768527" CREATED="1588018105879" MODIFIED="1588103986377">
+<icon BUILTIN="freemind_butterfly"/>
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Slack" ID="ID_1345977334" CREATED="1588103768886" MODIFIED="1588103986377">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Trello" ID="ID_445917738" CREATED="1588103748814" MODIFIED="1588103986377">
+<font NAME="Times New Roman" SIZE="14"/>
+</node>
+</node>
+<node TEXT="Challenges" POSITION="right" ID="ID_1597896197" CREATED="1588015786100" MODIFIED="1588103986377" STYLE="fork">
 <icon BUILTIN="full-6"/>
-<node CREATED="1588015796688" ID="ID_929069577" MODIFIED="1588103986377" TEXT="Point-and-click platforms">
 <font NAME="Times New Roman" SIZE="14"/>
+<edge STYLE="bezier"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Point-and-click platforms" ID="ID_929069577" CREATED="1588015796688" MODIFIED="1588103986377">
 <icon BUILTIN="list"/>
-</node>
-<node CREATED="1588015788946" ID="ID_1990896941" MODIFIED="1588270451749" TEXT="Machine Learning">
 <font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Machine Learning" ID="ID_1990896941" CREATED="1588015788946" MODIFIED="1588270451749">
 <icon BUILTIN="bookmark"/>
-</node>
-<node CREATED="1588016126866" ID="ID_129929691" MODIFIED="1588103986376" TEXT="Culture">
 <font NAME="Times New Roman" SIZE="14"/>
+</node>
+<node TEXT="Culture" ID="ID_129929691" CREATED="1588016126866" MODIFIED="1588103986376">
 <icon BUILTIN="group"/>
-<node CREATED="1588104533430" ID="ID_40865306" MODIFIED="1588244386910" TEXT="Overhead"/>
-<node CREATED="1588104541939" ID="ID_1553486513" MODIFIED="1588244394245" TEXT="Ownership"/>
+<font NAME="Times New Roman" SIZE="14"/>
+<node TEXT="Overhead" ID="ID_40865306" CREATED="1588104533430" MODIFIED="1588244386910"/>
+<node TEXT="Ownership" ID="ID_1553486513" CREATED="1588104541939" MODIFIED="1588244394245"/>
 </node>
 </node>
 </node>
